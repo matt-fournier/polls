91227c6860ad48681270efb3f833938faa3651e7
Matt Fournier <matt.fournier.dv@gmail.com>
2015-03-24 22:03:33 -0700

    first commit

---
+++ polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from polls.models import Choice,Question
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date information',    {'fields': ['pub_date'],'classes':['collapse']})
+
+    ]
+    inlines = [ChoiceInLine]
+
+
+admin.site.register(Question, QuestionAdmin)
